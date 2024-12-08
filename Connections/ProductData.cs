8db6a7ec0912be649b675611a4e51f7b4c8f167e
Irina Sviridova <sviridovairis@gmail.com>
2024-12-08 19:40:57 +0300

    Добавьте файлы проекта.

---
+++ Connections/ProductData.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Connections
+{
+    [Table("Shopping")]
+    class ProductData
+    {                
+        public int ProductCode { get;}
+        public string ProductName { get;}
+        public ProductData(int productCode, string productName) 
+        {           
+            ProductCode = productCode;
+            ProductName = productName;
+        }
+    }
+}
